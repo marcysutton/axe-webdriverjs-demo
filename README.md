94de674ad030d2a86374246d190806a82d259f95
Marcy Sutton <marcy.sutton@deque.com>
2016-02-11 13:35:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+#axe-webdriverjs demo
+
+How to set up aXe with Selenium WebdriverJS for automated testing.
+
+##Requirements
+* Node.js
+* Firefox browser
+
+##Installation
+
+From the command line, install axe-core:
+
+```
+npm install axe-core
+```
+
+Install required dependencies (includes selenium-webdriver):
+```
+npm install
+```
+
+Install Jasmine globally (may require sudo):
+```
+npm install -g jasmine
+jasmine init
+```
+
+##Usage
+
+```
+jasmine spec/test.js
+```
---
+++ spec/test.js
@@ -0,0 +1,39 @@
+var selenium = require('selenium-webdriver'),
+    AxeBuilder = require('axe-webdriverjs');
+
+describe('Selenium-aXe Tutorial', function() {
+
+    // Open the Deque website in the browser before each test is run
+    beforeEach(function(done) {
+        this.driver = new selenium.Builder().
+            withCapabilities(selenium.Capabilities.firefox()).
+            build();
+
+        this.driver.get('http://www.deque.com/').then(done);
+    });
+
+    // Close the website after each test is run (so that it is opened fresh each time)
+    afterEach(function(done) {
+        this.driver.quit().then(done);
+    });
+
+    // Test to ensure we are on the home page by checking the <body> tag for a specific class
+    it('Should be on the home page', function(done) {
+        var element = this.driver.findElement(selenium.By.tagName('body'));
+
+        element.getAttribute('class').then(function(className) {
+            expect(className).toContain('home');
+            done();
+        });
+    });
+
+    // Test accessibility with axe-core
+    it('Should have no accessibility violations', function(done) {
+        AxeBuilder(this.driver)
+            .analyze(function(results) {
+                expect(results.violations.length).toBe(0);
+                console.log(results.violations);
+                done();
+            })
+    });
+});
