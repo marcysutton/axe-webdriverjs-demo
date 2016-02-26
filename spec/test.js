ea2a49a9211c2a39d26ee65a52ba0682a26cf54e
Marcy Sutton <marcy.sutton@deque.com>
2016-02-25 16:19:50 -0800

    Make tests run with slow URLs

--- spec/test.js
+++ spec/test.js
@@ -1,43 +1,58 @@
 var selenium = require('selenium-webdriver'),
     AxeBuilder = require('axe-webdriverjs');
 
+var driver, browser;
+
 describe('Selenium-aXe Tutorial', function() {
 
     // Open the Deque website in the browser before each test is run
     beforeEach(function(done) {
-        this.driver = new selenium.Builder()
+        driver = new selenium.Builder()
             .forBrowser('firefox')
-            .build();
+            .usingServer('http://localhost:4444/wd/hub');
 
-        this.driver
-            .get('http://www.deque.com/')
-            .then(function() {
+        browser = driver.build();
+
+        browser.manage().timeouts().setScriptTimeout(60000);
+
+        browser.get('http://www.marcysutton.com/').then(function () {
+            browser.executeAsyncScript(function(callback) {
+                var script = document.createElement('script');
+                script.innerHTML = 'document.documentElement.classList.add("deque-axe-is-ready");';
+                document.documentElement.appendChild(script);
+                callback();
+            })
+            .then(function () {
+                return browser.wait(selenium.until.elementsLocated(selenium.By.css('.deque-axe-is-ready')));
+            })
+            .then(function(){
                 done();
             });
+        });
     });
 
     // Close the website after each test is run (so that it is opened fresh each time)
-    afterEach(function() {
-        this.driver.quit();
-    });
-
-    // Test to ensure we are on the home page by checking the <body> tag for a specific class
-    it('Should be on the home page', function(done) {
-        var element = this.driver.findElement(selenium.By.tagName('body'));
-
-        element.getAttribute('class').then(function(className) {
-            expect(className).toContain('home');
+    afterEach(function(done) {
+        browser.quit().then(function () {
             done();
         });
     });
 
-    // Test accessibility with axe-core
-    it('Should have no accessibility violations', function(done) {
-        AxeBuilder(this.driver)
-            .analyze(function(results) {
-                expect(results.violations.length).toBe(0);
-                console.log(results.violations);
-                done();
-            })
+    it('should just fetch the home page and analyze it', function (done) {
+        browser.findElement(selenium.By.css('body.home')).then(function (element) {
+            return element.getAttribute('class').then(function (className) {
+                expect(className.indexOf('home')).toBe(0);
+            });
+        }).then(function () {
+            AxeBuilder(browser)
+                .analyze(function(results) {
+                    console.log('Accessibility Violations: ', results.violations.length);
+                    if (results.violations.length > 0) {
+                        console.log(results.violations);
+                    }
+                    expect(results.violations.length).toBe(0);
+                    done();
+                })
+        });
     });
 });
