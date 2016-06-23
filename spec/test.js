48f78d93f4a3616de273f9e51c82748fdd1e68d8
marcysutton <holla@marcysutton.com>
2016-06-23 11:42:21 -0700

    Modified test for HillaryClinton.com

--- spec/test.js
+++ spec/test.js
@@ -1,16 +1,34 @@
 var selenium = require('selenium-webdriver'),
     AxeBuilder = require('axe-webdriverjs');
 
-describe('Selenium-aXe Tutorial', function() {
+var util = require('util');
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+describe('Homepage accessibility', function() {
 
     // Open the Deque website in the browser before each test is run
     beforeEach(function(done) {
+        jasmine.addMatchers({
+            toHaveNoViolations: function() {
+                return {
+                    compare: function(result) {
+                        var violations = result.violations.length;
+                        return {
+                            message:  "Expected no accessibility violations but found " + violations,
+                            pass: violations == 0
+                        }
+                    }
+                }
+            }
+        });
+
         this.driver = new selenium.Builder()
             .forBrowser('firefox')
             .build();
 
         this.driver
-            .get('http://www.deque.com/')
+            .get('http://www.hillaryclinton.com/')
             .then(function() {
                 done();
             });
@@ -23,7 +41,7 @@ describe('Selenium-aXe Tutorial', function() {
 
     // Test to ensure we are on the home page by checking the <body> tag for a specific class
     it('Should be on the home page', function(done) {
-        var element = this.driver.findElement(selenium.By.tagName('body'));
+        var element = this.driver.findElement(selenium.By.css('.homepage'));
 
         element.getAttribute('class').then(function(className) {
             expect(className).toContain('home');
@@ -35,8 +53,10 @@ describe('Selenium-aXe Tutorial', function() {
     it('Should have no accessibility violations', function(done) {
         AxeBuilder(this.driver)
             .analyze(function(results) {
-                expect(results.violations.length).toBe(0);
-                console.log(results.violations);
+                // if (results.violations.length > 0) {
+                //     console.log(util.inspect(results.violations, true, null));
+                // }
+                expect(results).toHaveNoViolations();
                 done();
             })
     });
